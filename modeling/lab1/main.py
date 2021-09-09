8e25fee69ccf3ceff7d7048f89c2b574f3b48c00
Alex Basko <61543351+albaSANDROS@users.noreply.github.com>
2021-09-09 21:06:48 +0300

    Update main.py

--- modeling/lab1/main.py
+++ modeling/lab1/main.py
@@ -2,54 +2,67 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sequence = []
-def calculate_sequence(a, m, Rn, N):
-    for x in range(0, N):
+
+
+def made_sequence(a, Rn, m, N):
+    for x in range (0, N):
         Rn = a * Rn % m
-        sequence.append(Rn / m)
+        sequence.append(Rn/m)
 
-
-def show_hist():
+def show_histogram():
     weights = np.ones_like(sequence) / float(len(sequence))
     plt.hist(sequence, bins=np.linspace(0, 1, 21), weights=weights, histtype='bar', color='blue', rwidth=0.95)
     plt.hlines(1/20, 0, 1)
     plt.show()
 
 
-def calculate_implicit_criteria():
-    criteria_matchers = 0
+def indirect_check():
+    amount_of_K_pairs = 0
     for i in range(0, len(sequence), 2):
         if sequence[i] ** 2 + sequence[i + 1] ** 2 < 1:
-            criteria_matchers += 1
-    print('\nРеальность: ', 2 * criteria_matchers / len(sequence), '\nОжидание: ', np.pi / 4)
+            amount_of_K_pairs += 1
+    print('\nОпытный: ', 2 * amount_of_K_pairs / len(sequence), '\nТеоретический: ', np.pi / 4)
 
 
-def calculate_period():
-    x_v_index_matchers = []
+def period_length():
+    indexes = []
     for i in range(0, len(sequence)):
         if sequence[i] == sequence[-1]:
-            x_v_index_matchers.append(i)
-    return x_v_index_matchers[1] - x_v_index_matchers[0]
+            indexes.append(i)
+    if len(indexes) == 1:
+        indexes.append(indexes[0])
+    return indexes[1] - indexes[0]
 
 
-def calculate_aperiodic_inteval(P):
-    i_3 = 0
-    while sequence[i_3] != sequence[i_3 + P]:
-        i_3 += 1
-    return P + i_3
-# a = 134279
-# r0 = 1
-# m = 313107
-# n = 1000000
-R = int(input("R0:"))
-a = int(input("a: "))
-m = int(input("m: "))
-calculate_sequence(a, m, R, 1000000)
-print('\nМат.ожидание: ', np.mean(sequence), '\nДисперсия: ', np.var(sequence), '\nОтклонение: ', np.std(sequence))
+def aperiodic_section_length(P):
+    index = 0
+    while sequence[index] != sequence[index + P]:
+        index += 1
+        if (index + P == len(sequence) - 1):
+            StopIteration
 
-calculate_implicit_criteria()
+    return index + P
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
 
-period = calculate_period()
-aperiodic_interval = calculate_aperiodic_inteval(period)
-print('\nПериод: ', period, '\nАпериодический интервал: ', aperiodic_interval)
-show_hist()
+
+    R = int(input("R0:"))
+    a = int(input("a: "))
+    m = int(input("m: "))
+    if R < 0:
+        print('\nВы вводите отрицательный R')
+    elif a < 0:
+        print('\nВы вводите отрицательный a')
+    elif m < 0:
+        print('\nВы вводите отрицательный m')
+    elif m < a:
+        print('\nm должен быть больше чем a')
+    else:
+        made_sequence(a, R, m, 1000000)
+        print('\nМатематическое ожидание: ', np.mean(sequence), '\nДисперсия: ', np.var(sequence), '\nСреднеквадратичное отклонениеS: ', np.std(sequence))
+        indirect_check()
+        period = period_length()
+        aperiodic_section = aperiodic_section_length(period)
+        print('\nПериод: ', period, '\nАпериодический интервал: ', aperiodic_section)
+        show_histogram()
 
