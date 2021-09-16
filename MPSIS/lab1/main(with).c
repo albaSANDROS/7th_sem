71eb0498d1afd7979c183c8619cfa2bf93dfb8ea
Alex Basko <61543351+albaSANDROS@users.noreply.github.com>
2021-09-16 08:46:48 +0300

    Update main(with).c

--- MPSIS/lab1/main(with).c
+++ MPSIS/lab1/main(with).c
@@ -1,105 +1,88 @@
-// uncorrect var!
-
 #include <msp430.h> 
 
+int b1_prev_state = BIT7;
+int b2_prev_state = BIT2;
+int l1_state = 0;
 
-int LED4 = 0; //диод 4 : 0 - не горит 1 -горит
-int LED5 = 0; //диод 5 : 0 - не горит 1 -горит
+void sleep() {
+	volatile int i = 0;
+	for	(i = 0; i < 1000; i++) {}
+}
 
-int buttonS1 = 0;
-int buttonS2 = 0;
-// 1 - нажата
-// 0 - не нажата
+#pragma vector = PORT1_VECTOR
+__interrupt void button1(void) {
+	sleep();
 
+	int b1_cur_state = (P1IN & BIT7);
 
-// обработчик прерываний
-#pragma vector = PORT1_VECTOR  //для порта 1
-__interrupt void buttonPush1(void) {
+	if (b1_prev_state == 0 && b1_cur_state == BIT7) {
+   		P1OUT |= BIT2;
+   	}
 
+   	if (b1_prev_state == BIT7 && b1_cur_state == 0 && b2_prev_state == 0) {
+   	    P1OUT &= ~BIT2;
+   	}
 
+   	b1_prev_state = b1_cur_state;
 
+	P1IES ^= BIT7;
+	P1IFG = 0;
+}
 
-	if (buttonS1 == 0) { //кнока нажата
-		if(LED5 == 0 && buttonS2 == 0){
-			P1OUT |= BIT2;
-			LED5 = 1;
+#pragma vector = PORT2_VECTOR
+__interrupt void button2(void) {
+	sleep();
+
+	int b2_cur_state = (P2IN & BIT2);
+
+	if (b1_prev_state == BIT7 && b2_prev_state == 0 && b2_cur_state == BIT2) {
+			if (l1_state == 0){
+		    	P1OUT |= BIT3;
+		    	l1_state = 1;
+		    } else {
+		    	if (l1_state == 1){
+		    		P1OUT &= ~BIT3;
+		    		l1_state = 0;
+		    	}
+		    }
 		}
-        P1IES &= ~BIT7; //реагирование только на фронт
-        buttonS1 = 1;
 
-    } else {
+	b2_prev_state = b2_cur_state;
 
-        P1IES |= BIT7; //реагирование только на спад
-        buttonS1 = 0;
-    }
-
-
-
-   // volatile int i = 0;
-   // for (i = 0; i < 1000; i++) {}
-    P1IFG = 0;
+	P2IES ^= BIT2;
+	P2IFG = 0;
 }
 
-#pragma vector = PORT2_VECTOR  //для порта 2
-__interrupt void buttonPush2(void) {
 
-	if (buttonS2 == 0) { //кнока нажата
-        P2IES &= ~BIT2; //реагирование только на фронт
-        buttonS2 = 1;
-        if(buttonS1 == 1 && LED4 == 0){
-        	P1OUT |= BIT1;
-        	LED4 = 1;
-        }
-    } else {
-    	if(LED5 == 1){
-    		P1OUT &= ~BIT2;
-    		LED5 = 0;
-    	}
-
-        P2IES |= BIT2; //реагирование только на спад
-        buttonS2 = 0;
-
-        if(buttonS1 == 1 && LED4 == 1){
-        	 P1OUT &= ~BIT1;
-        	 LED4 = 0;
-        }
-    }
-
-  //  volatile int i = 0;
-  //  for (i = 0; i < 1000; i++) {}
-    P2IFG = 0;
-}
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer (отключение сторожевого таймера)
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	
+    //LED1 - 1.3
+    //LED2 - 1.2
 
+    P1DIR = BIT2 | BIT3;
     P1OUT = 0;
-    P2OUT = 0;
 
-    P1SEL = 0;
-    P2SEL = 0;
-
-	P1DIR |= (BIT1 | BIT2);	// 1 - выход 0 - вход (2 дтода 4 и 5 - 1.1 и 1.2)
-
+    P1REN |= BIT7;
     P1OUT |= BIT7;
+
+    P2REN |= BIT2;
     P2OUT |= BIT2;
 
-    P1REN |= BIT7; // от помех на кнопке S1
-    P2REN |= BIT2; // от помех на кнопке S2
+    P1IES |= BIT7;
+    P2IES |= BIT2;
 
-	P1IE |= BIT7; // разрешение прерывания S1
-	P2IE |= BIT2; // разрешение прерывания S2
-
-    __bis_SR_register(GIE);
+    P1IFG = 0;
+    P2IFG = 0;
 
     P1IE |= BIT7;
     P2IE |= BIT2;
-    P1IES |= BIT7;
-    P2IES |= BIT2;
-    P1IFG = 0;
-    P2IFG = 0;
+
+    __bis_SR_register(LPM0_bits + GIE);
 
     __no_operation();
 
-	return 0;
+    return 0;
 }
+
