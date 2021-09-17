7629dccdbaa2640810ab3de246b8e971a7cae267
Alex Basko <61543351+albaSANDROS@users.noreply.github.com>
2021-09-17 20:02:47 +0300

    Update main(with).c

--- MPSIS/lab1/main(with).c
+++ MPSIS/lab1/main(with).c
@@ -5,59 +5,60 @@ int b2_prev_state = BIT2;
 int l1_state = 0;
 
 void sleep() {
-	volatile int i = 0;
-	for	(i = 0; i < 1000; i++) {}
+    volatile int i = 0;
+    for (i = 0; i < 1000; i++) {}
 }
 
 #pragma vector = PORT1_VECTOR
 __interrupt void button1(void) { //LEDB S1 using
-	sleep();
+    sleep();
 
-	int b1_cur_state = (P1IN & BIT7);
+    int b1_cur_state = (P1IN & BIT7);
+    int b2_cur_state = (P2IN & BIT2);
 
-	if (b1_prev_state == 0 && b1_cur_state == BIT7) {
-   		P1OUT |= BIT2;
-   	}
+    if (b1_prev_state == 0 && b1_cur_state == BIT7) {
+        P1OUT |= BIT2;
+    }
 
-   	if (b1_prev_state == BIT7 && b1_cur_state == 0 && b2_prev_state == 0) {
-   	    P1OUT &= ~BIT2;
-   	}
+    if (b1_prev_state == BIT7 && b1_cur_state == 0 && b2_prev_state == 0 && b2_cur_state == 0) {
+        P1OUT &= ~BIT2;
+    }
 
-   	b1_prev_state = b1_cur_state;
+    b1_prev_state = b1_cur_state;
 
-	P1IES ^= BIT7; //xor to BIT7 to get changes
-	P1IFG = 0;
+    P1IES ^= BIT7; //xor to BIT7 to get changes
+    P1IFG = 0;
 }
 
 #pragma vector = PORT2_VECTOR
 __interrupt void button2(void) {
-	sleep();
+    sleep();
+    int b1_cur_state = (P1IN & BIT7);
+    int b2_cur_state = (P2IN & BIT2);
 
-	int b2_cur_state = (P2IN & BIT2);
+    if (b1_prev_state == BIT7 && b1_cur_state == BIT7 && b2_prev_state == 0 && b2_cur_state == BIT2) {
+            if (l1_state == 0){
+                P1OUT |= BIT3;
+                l1_state = 1;
+            } else {
+                if (l1_state == 1){
+                    P1OUT &= ~BIT3;
+                    l1_state = 0;
+                }
+            }
+        }
 
-	if (b1_prev_state == BIT7 && b2_prev_state == 0 && b2_cur_state == BIT2) {
-			if (l1_state == 0){
-		    	P1OUT |= BIT3;
-		    	l1_state = 1;
-		    } else {
-		    	if (l1_state == 1){
-		    		P1OUT &= ~BIT3;
-		    		l1_state = 0;
-		    	}
-		    }
-		}
+    b2_prev_state = b2_cur_state;
 
-	b2_prev_state = b2_cur_state;
-
-	P2IES ^= BIT2;
-	P2IFG = 0;
+    P2IES ^= BIT2;
+    P2IFG = 0;
 }
 
 
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	
+    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog timer
+
     //LED1 - 1.3
     //LED2 - 1.2
 
@@ -85,4 +86,3 @@ int main(void) {
 
     return 0;
 }
-
