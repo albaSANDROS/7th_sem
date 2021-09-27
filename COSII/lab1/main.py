95f34c04e9626b41d2b2a9ee48741fe1d706cc79
Alex Basko <61543351+albaSANDROS@users.noreply.github.com>
2021-09-27 23:37:54 +0300

    Update main.py

--- COSII/lab1/main.py
+++ COSII/lab1/main.py
@@ -1,14 +1,16 @@
 from PIL import Image
+from django.db.models.functions import math
 from numpy import asarray
 import seaborn as sn
 import cv2
 import numpy as np
+import math
 from matplotlib import pyplot as plt
 
 
 def to_one_arr(source):
     output = []
-    for i in range(255):
+    for i in range(256):
         output.append(0)
 
     for x in range(len(source)):
@@ -21,48 +23,14 @@ def to_one_arr(source):
     return output
 
 
-def element_v(source):
-    f = 230
-    g = 100
-
-    output = np.copy(source)
-    for x in range(len(source)):
-        for y in range(len(source[0])):
-            for z in range(len(source[0][0])):
-                pixel = output[x][y][z]
-                if pixel < f:
-                    pixel = 0
-                else:
-                    pixel = (g / (255 - f)) * (pixel - f)
-                output[x][y][z] = pixel
-    return output
-
-
-def element_g(source):
-    f = 110
-    g = 20
-
-    output = np.copy(source)
-    for x in range(len(source)):
-        for y in range(len(source[0])):
-            for z in range(len(source[0][0])):
-                pixel = output[x][y][z]
-                if pixel > f:
-                    pixel = 255
-                else:
-                    pixel = ((255 - g) / f) * pixel + g
-                output[x][y][z] = pixel
-    return output
-
-
-def previt(source):
+def roberts(source):
     output = np.copy(source)
     for x in range(1, len(source) - 1):
         for y in range(1, len(source[0]) - 1):
             for z in range(len(source[0][0])):
-                h1 = - int(source[x - 1][y - 1][z]) - source[x - 1][y][z] - source[x - 1][y + 1][z] + source[x + 1][y - 1][z] + source[x + 1][y][z] + source[x + 1][y + 1][z]
-                h2 = - int(source[x - 1][y - 1][z]) - source[x][y - 1][z] - source[x + 1][y - 1][z] + source[x - 1][y + 1][z] - source[x][y + 1][z] - source[x + 1][y + 1][z]
-                h = max(h1, h2)
+                h1 = int(source[x + 1][y][z]) - source[x][y + 1][z]
+                h2 = int(source[x][y][z]) - source[x + 1][y + 1][z]
+                h = math.sqrt((math.pow(h1, 2) + math.pow(h2, 2)))
                 if h < 0:
                     h = 0
                 elif h > 255:
@@ -71,57 +39,26 @@ def previt(source):
     return output
 
 
+def corr(image2):
+    c = 255 / np.log(1 + np.max(image2))
+    log_image = c * (np.log(image2 + 1))
+    log_image = np.array(log_image, dtype=np.uint8)
+    return log_image
+
+
 if __name__ == '__main__':
     image = Image.open("img.jpg")
+    imagecv = cv2.imread('bad.jpg')
 
     plt.plot(to_one_arr(asarray(image)))
     plt.show()
 
     array = asarray(image)
-    v = element_v(array)
-    g = element_g(array)
-    pr = previt(array)
+    pr = roberts(array)
 
-    plt.plot(to_one_arr(v))
-    plt.show()
-    plt.plot(to_one_arr(g))
-    plt.show()
     plt.plot(to_one_arr(pr))
     plt.show()
 
     Image.fromarray(array).save("default.jpg")
-
-    Image.fromarray(pr).save("previt.jpg")
-    Image.fromarray(v).save("v.jpg")
-    Image.fromarray(g).save("g.jpg")
-    Image.fromarray(element_g(asarray(Image.open("bad.jpg")))).save("good.jpg")
-
-
-
-
-@staticmethod
-    def roberts_operator(image):
-        im = image.copy()
-        pixels = im.load()
-        w, h = im.size
-
-        for i in range(w - 1):
-            for j in range(h - 1):
-                pixels[i, j] = tuple(
-                    map(lambda x1, x2, x3, x4: int(math.sqrt((x1 - x2) ** 2 + (x2 - x3) ** 2)), pixels[i + 1, j],
-                        pixels[i, j + 1], pixels[i, j], pixels[i + 1, j + 1]))
-
-        return im
-
-
-
-    @staticmethod
-    def logarithmic_correction(image):
-        constant = input('Enter constant:')
-        im = image.copy()
-        pixels = im.load()
-
-        for i in range(im.size[0]):
-            for j in range(im.size[1]):
-                pixels[i, j] = tuple(map(lambda x: int(constant * math.log(1 + x)), pixels[i, j]))
-        return im
+    Image.fromarray(pr).save("roberts.jpg")
+    Image.fromarray(corr(imagecv)).save("good.jpg")
