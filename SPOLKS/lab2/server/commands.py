7b94dd3f6ab514b69f12fbf211a518507971b307
Alex Basko <61543351+albaSANDROS@users.noreply.github.com>
2021-11-30 21:19:45 +0300

    Create commands.py

---
+++ SPOLKS/lab2/server/commands.py
@@ -0,0 +1,24 @@
+help_list = {
+    "ECHO": "return typed string after this command",
+    "TIME": "show server time",
+    "EXIT": "close server connection",
+    "SHOW_CLIENTS": "show info about clients",
+    "HELP": "help list"
+}
+
+server_commands = {
+    "ECHO": "echo",
+    "TIME": "time",
+    "EXIT": "exit",
+    "HELP": "help",
+    "SHOW_CLIENTS": "show_clients"
+}
+
+client_commands = {
+    "GET": "download",
+    "POST": "upload",
+    "DELETE": "delete",
+    "ECHO": "echo",
+    "TIME": "time",
+    "EXIT": "exit"
+}
