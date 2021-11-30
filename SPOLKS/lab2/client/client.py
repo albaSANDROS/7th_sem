56e6d5ea8626c9292b54d176163e0a39e51a91f7
Alex Basko <61543351+albaSANDROS@users.noreply.github.com>
2021-11-30 21:49:15 +0300

    Update client.py

--- SPOLKS/lab2/client/client.py
+++ SPOLKS/lab2/client/client.py
@@ -3,32 +3,32 @@ from commands import client_commands
 import os
 import os.path
 import sys
-#from datetime import datetime
-from time import time
+
+
 import time
 import re
 
 PORT = 9001
 
+WINDOW_SIZE = 4096
+
 BUFFER_SIZE = 1024
-OOB_RATE = 10000
 TIMEOUT = 20
 
 OK_STATUS = 200
 
+UPLOAD_PROGRESS = 0
+DOWNLOAD_PROGRESS = 0
 
-def wait_ok():
-    while (client.recv(2).decode('utf-8') != "OK"):
-        print("wait for OK")
 
-def send_ok():
-    client.send("OK".encode('utf-8'))
 
 def get_data():
-    return client.recv(BUFFER_SIZE).decode('utf-8')
+    data, address = client.recvfrom(BUFFER_SIZE)
+    data = data.decode('utf-8')
+    return [data, address]
 
 def send_data(data):
-    client.send(str(data).encode('utf-8'))
+    client.sendto(str(data).encode('utf-8'), server_address)
 
 
 def handle_input_request(request):
@@ -65,11 +65,6 @@ def handle_input_request(request):
         else:
             show_error_message("No such file exists")
 
-    if (client_commands.get(name_command) == "delete"):
-        send_data(request)
-        if (wait_for_ack(name_command) == False):
-            return
-        delete(body, request)
 
     if (client_commands.get(name_command) == "exit"):
         send_data(request)
@@ -80,7 +75,7 @@ def handle_input_request(request):
 
 def wait_for_ack(command_to_compare):
     while True:
-        response = client.recv(BUFFER_SIZE).decode('utf-8').split(" ", 2)
+        response = get_data()[0].split(" ", 2)
 
         if not response:
             return False
@@ -109,9 +104,9 @@ def is_server_available(request, command):
 
     while(i > 0):
         try:
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect((HOST, PORT))
-            client.send(request.encode('utf-8'))
+            client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            client.connect(server_address)
+            client.sendto(request.encode('utf-8'), server_address)
             wait_for_ack(command)
             return True
 
@@ -132,60 +127,65 @@ def is_file_exist(file_name):
     return os.path.exists(file_name)
 
 def echo():
-    print(get_data())
+    print(get_data()[0])
 
 def get_time():
-    print(get_data())
+    print(get_data()[0])
+
+def handle_disconnect():
+    print("wait server")
+    time.sleep(1)
 
 def download(file_name, request):
-    size = int(get_data()) #1
+    global WINDOW_SIZE
 
-    send_ok() #2
+    send_data(WINDOW_SIZE)
 
-    send_data(0) #3
+    WINDOW_SIZE = int(get_data()[0])
+    server_window = WINDOW_SIZE
 
-    data_size_recv = int(get_data()) #4
+    size = int(get_data()[0])
 
-    send_ok() #5
+    send_data(DOWNLOAD_PROGRESS)
+
+    data_size_recv = int(get_data()[0])
 
     if (data_size_recv == 0):
         f = open(file_name, "wb")
     else:
         f = open(file_name, "rb+")
 
+    current_pos = data_size_recv
 
-    time_start = time.time()
+    i = 0
     progress_bar = 10
-    while (data_size_recv < size):
+    while (1):
         try:
+            data = client.recvfrom(BUFFER_SIZE)[0]
+            if data:
+                if data == b'EOF':
+                    break
+                else:
+                    i += 1
+                    f.seek(current_pos, 0)
+                    f.write(data)
+                    current_pos += len(data)
+                    server_window = server_window - len(data)
+                    if (server_window == 0):
+                        server_window = WINDOW_SIZE
+                        send_data(current_pos)
+            else:
+                print("Server disconnected")
+                return
 
-            data = client.recv(BUFFER_SIZE)
-
-
-            f.seek(data_size_recv, 0)
-            f.write(data)
-            data_size_recv += BUFFER_SIZE
-
-            progress = (data_size_recv / size) * 100
+            progress = (current_pos / size) * 100
             if (progress >= progress_bar):
                 print("Download progress: %d%% " % progress)
                 progress_bar += 10
 
-        except socket.error as e:
-            if(is_server_available(request, "download")):
-                size = int(get_data())
-                send_ok()
-                send_data(data_size_recv)
-                data_size_recv = int(get_data())
-                send_ok()
-                print("\n")
-            else:
-                f.close()
-                client.close()
-                os._exit(1)
-
         except KeyboardInterrupt:
             print("KeyboardInterrupt was handled")
+            send_data("ERROR")
             f.close()
             client.close()
             os._exit(1)
@@ -193,94 +193,61 @@ def download(file_name, request):
     f.close()
     print("\n" + file_name + " was downloaded")
 
-    time_end = time.time()
-
-    delta_time = (time_end - time_start)
-
-    print("Total time: %f ms" %delta_time)
-
-    speed = (size/1024**2)/delta_time
-
-    print("Average speed: %f m/s" % speed)
-
 def upload(file_name, request):
+    global WINDOW_SIZE
 
     f = open (file_name, "rb+")
 
     size = int(os.path.getsize(file_name))
 
-    send_data(size)
+    send_data(WINDOW_SIZE)
 
-    wait_ok()
+    WINDOW_SIZE = int(get_data()[0])
+    server_window = WINDOW_SIZE
+
+    send_data(size)
 
     send_data(0)
 
-    data_size_recv = int(get_data())
+    data_size_recv = int(get_data()[0])
 
-    wait_ok()
+    current_pos = data_size_recv
 
-    f.seek(data_size_recv, 0)
+    f.seek(current_pos, 0)
 
-    time_start = time.time()
-
-    oob_count_send = 0
     progress_bar = 10
-    while (data_size_recv < size):
+    while (1):
         try:
+            if (current_pos >= size):
+                client.sendto(b"EOF", server_address)
+                break
+            else:
+                data_file = f.read(BUFFER_SIZE)
+                client.sendto(data_file, server_address)
+                current_pos = current_pos + BUFFER_SIZE
+                f.seek(current_pos)
 
-            data_file = f.read(BUFFER_SIZE)
-            if (oob_count_send == OOB_RATE):
-                client.send('*'.encode('utf-8'), socket.MSG_OOB)
-                oob_count_send = 0
-            client.send(data_file)
-            oob_count_send += 1
+            server_window = server_window - BUFFER_SIZE
+            if (server_window == 0):
+                server_window = WINDOW_SIZE
+                send_data(current_pos)
 
-            progress = (data_size_recv / size) * 100
+            progress = (current_pos / size) * 100
             if (progress >= progress_bar):
-                print("Download progress: %d%% " % progress)
+                print("Upload progress: %d%% " % progress)
                 progress_bar += 10
 
-            data_size_recv += BUFFER_SIZE
-            f.seek(data_size_recv, 0)
-
-        except socket.error as e:
-            if(is_server_available(request, "upload")):
-                send_data(size)
-                wait_ok()
-                send_data(data_size_recv)
-                data_size_recv = int(get_data())
-                wait_ok()
-                print("\n")
-            else:
-                f.close()
-                client.close()
-                os._exit(1)
-
         except KeyboardInterrupt:
-            print("KeyboardInterrupt was handled")
+            print("KeyboardInterrupt handled")
+            client.sendto(b"ERROR", server_address)
             f.close()
             client.close()
             os._exit(1)
 
 
-
-
     f.close()
     print("\n" + file_name + " was uploaded")
 
-    time_end = time.time()
-
-    delta_time = (time_end - time_start)
-
-    print("Total time: %f ms" %delta_time)
-
-    speed = (size/1024**2)/delta_time
-
-    print("Average speed: %f m/s" % speed)
-
-
-def delete(file_name):
-    pass
 
 def exit():
     pass
@@ -322,18 +289,12 @@ while (is_valid_address == False):
 
 show_start_message()
 server_address = (HOST, PORT)
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.connect(server_address)
 
 
-while True:
 
-    try:
-        request = input()
-        if (check_valid_request(request)):
-            handle_input_request(request)
-    except KeyboardInterrupt:
-        print("KeyboardInterrupt was handled")
-        client.close()
-        os._exit(1)
+
+while True:
+    request = input()
+    handle_input_request(request)
